a7fb14b320d42d6dd50ea504857f931082095634
leonsuzuki0719 <leonsuzuki0719@gmail.com>
2021-07-07 10:25:53 +0200

    setup the project architecture

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" //match any network id
+    },
+  },
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/abis',
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }  
+    }
+  }
+};
